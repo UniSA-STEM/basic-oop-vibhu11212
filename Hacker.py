513be483395aefb711f89e6ddeb2bf3f8c18e916
vibhu karthikeya <potvy001@mymail.unisa.edu.au>
2025-10-20 19:12:47 +1030

    Indentation error fixed, previosly methods were in init constructer

--- Hacker.py
+++ Hacker.py
@@ -15,83 +15,108 @@ class Hacker:
         self.__rigs = None
         self.__trace_level = 0
 
-        def get_name(self):
-            return self.__name
+    def get_name(self):
+        return self.__name
 
-        def get_inventory(self):
-            return self.__inventory
+    def get_inventory(self):
+        return self.__inventory
 
-        def get_rigs(self):
-            return self.__rigs
+    def get_rigs(self):
+        return self.__rigs
 
-        def set_rigs(self, rigs):
-            self.__rigs = rigs
+    def set_rigs(self, rigs):
+        self.__rigs = rigs
 
-        def get_trace_level(self):
-            return self.__trace_level
+    def get_trace_level(self):
+        return self.__trace_level
 
-        def set_trace_level(self, trace_level):
-            self.__trace_level = trace_level
+    def set_trace_level(self, trace_level):
+        self.__trace_level = trace_level
 
-        # properties for instance attributes
-        name = property(get_name)
-        inventory = property(get_inventory)
-        rigs = property(get_rigs, set_rigs)
-        trace_level = property(get_trace_level, set_trace_level)
+    # properties for instance attributes
+    name = property(get_name)
+    inventory = property(get_inventory)
+    rigs = property(get_rigs, set_rigs)
+    trace_level = property(get_trace_level, set_trace_level)
 
-        def aquire_a_rig(self, rig):  # DONE
-            if self.scan_and_remove_from_inventory("CryptoToken"):
-                self.rig = rig
-                print(f"{self.name} has activated rig {self.rig.name}")
-            else:
-                print("No Crypto Tokens")
+    def aquire_a_rig(self, rig):  # DONE
+        if self.scan_and_remove_from_inventory("CryptoToken"):
+            self.rig = rig
+            print(f"{self.name} has activated rig {self.rig.name}")
+        else:
+            print("No Crypto Tokens")
 
-        def scan_and_remove_from_inventory(self, name):  # DONE
+    def scan_and_remove_from_inventory(self, name):  # DONE
+        for asset in self.inventory:
+            if asset.asset_name == name:
+                self.inventory.remove(asset)
+                return asset
+        return None
+
+    def display_trace_warning(self):
+
+        if self.trace_level > 5:
+            print(
+                f"WARNING: {self.name} is exposed! Trace level is {self.trace_level}."
+                f" Risky actions are now blocked.")
+
+    def check_trace_threshold(self):
+        if self.trace_level > 5:
+            print(f"Trace level for {self.name} is too high! ")
+            return False
+        return True
+
+    def launch_data_spike(self, other_rig):
+        if self.check_trace_threshold() and self.rig:
+            if self.rig.release_from_rig("Data Spike"):
+                other_rig.take_hit()
+                self.trace_level += 1
+                self.display_trace_warning()
+
+    def extract_assets(self, other_rig):
+        if self.check_trace_threshold() and self.scan_and_remove_from_inventory(
+                "Removable Drive"):
+            if other_rig.broken_state:
+                assets_to_extract = [asset for asset in
+                                     other_rig.storage if
+                                     not asset.encrypted]
+                for asset in assets_to_extract:
+                    extracted = other_rig.release_from_rig(
+                        asset.asset_name)
+                    if extracted:
+                        self.inventory.append(extracted)
+                self.trace_level += 1
+                self.display_trace_warning()
+
+    def encrypt_inventory_asset(self, asset_name):  # encrypt in inventory
+        chip = self.scan_and_remove_from_inventory("Security Chip")
+        if chip:
             for asset in self.inventory:
-                if asset.asset_name == name:
-                    self.inventory.remove(asset)
-                    return asset
-            return None
+                if asset.asset_name == asset_name:
+                    asset.encrypted = True
+                    return "Encrypted"
+            self.inventory.append(chip)
+        else:
+            return "no security chip"
 
-        def display_trace_warning(self):
+    def decrypt_inventory_asset(self, asset_name):  # decryppt in inventory
+        chip = self.scan_and_remove_from_inventory("Security Chip")
+        if chip:
+            for asset in self.inventory:
+                if asset.asset_name == asset_name:
+                    asset.encrypted = False
+                    return "Decrypted"
+            self.inventory.append(chip)
+        else:
+            return "no security chip"
 
-            if self.trace_level > 5:
-                print(
-                    f"WARNING: {self.name} is exposed! Trace level is {self.trace_level}."
-                    f" Risky actions are now blocked.")
-
-        def check_trace_threshold(self):
-            if self.trace_level > 5:
-                print(f"Trace level for {self.name} is too high! ")
-                return False
-            return True
-
-        def launch_data_spike(self, other_rig):
-            if self.check_trace_threshold() and self.rig:
-                if self.rig.release_from_rig("Data Spike"):
-                    other_rig.take_hit()
-                    self.trace_level += 1
-                    self.display_trace_warning()
-
-        def extract_assets(self, other_rig):
-            if self.check_trace_threshold() and self.scan_and_remove_from_inventory(
-                    "Removable Drive"):
-                if other_rig.broken_state:
-                    assets_to_extract = [asset for asset in
-                                         other_rig.storage if
-                                         not asset.encrypted]
-                    for asset in assets_to_extract:
-                        extracted = other_rig.release_from_rig(
-                            asset.asset_name)
-                        if extracted:
-                            self.inventory.append(extracted)
-                    self.trace_level += 1
-                    self.display_trace_warning()
-
-        def encrypt_inventory_asset(self, asset_name):  # encrypt in inventory
+    def encrypt_rig_asset(self, asset_name):  # encrypt in rig storage
+        if not self.rig:
+            return "no rig"
+        else:
             chip = self.scan_and_remove_from_inventory("Security Chip")
             if chip:
-                for asset in self.inventory:
+                for asset in self.rig.storage:
                     if asset.asset_name == asset_name:
                         asset.encrypted = True
                         return "Encrypted"
@@ -99,10 +124,13 @@ class Hacker:
             else:
                 return "no security chip"
 
-        def decrypt_inventory_asset(self, asset_name):  # decryppt in inventory
+    def decrypt_rig_asset(self, asset_name):  # decrypt in rig storage
+        if not self.rig:
+            return "no rig"
+        else:
             chip = self.scan_and_remove_from_inventory("Security Chip")
             if chip:
-                for asset in self.inventory:
+                for asset in self.rig.storage:
                     if asset.asset_name == asset_name:
                         asset.encrypted = False
                         return "Decrypted"
@@ -110,59 +138,31 @@ class Hacker:
             else:
                 return "no security chip"
 
-        def encrypt_rig_asset(self, asset_name):  # encrypt in rig storage
-            if not self.rig:
-                return "no rig"
-            else:
-                chip = self.scan_and_remove_from_inventory("Security Chip")
-                if chip:
-                    for asset in self.rig.storage:
-                        if asset.asset_name == asset_name:
-                            asset.encrypted = True
-                            return "Encrypted"
-                    self.inventory.append(chip)
-                else:
-                    return "no security chip"
+    def upgrade_rigs(self):
+        if self.check_trace_threshold():
+            if self.rig and self.scan_and_remove_from_inventory(
+                    "Hardware Patch"):
+                self.rig.upgrade()
+                self.trace_level += 1
+                self.display_trace_warning()
 
-        def decrypt_rig_asset(self, asset_name):  # decrypt in rig storage
-            if not self.rig:
-                return "no rig"
-            else:
-                chip = self.scan_and_remove_from_inventory("Security Chip")
-                if chip:
-                    for asset in self.rig.storage:
-                        if asset.asset_name == asset_name:
-                            asset.encrypted = False
-                            return "Decrypted"
-                    self.inventory.append(chip)
-                else:
-                    return "no security chip"
-
-        def upgrade_rigs(self):
-            if self.check_trace_threshold():
-                if self.rig and self.scan_and_remove_from_inventory(
-                        "Hardware Patch"):
-                    self.rig.upgrade()
-                    self.trace_level += 1
-                    self.display_trace_warning()
-
-        def repair_rig(self):
-            if self.scan_and_remove_from_inventory("CryptoToken"):
-                if self.rig:
-                    self.rig.repair()
-            else:
-                print(f"{self.name} has no CryptoToken to perform repairs.")
-
-        def move_to_rig(self, name):  # DONE
+    def repair_rig(self):
+        if self.scan_and_remove_from_inventory("CryptoToken"):
             if self.rig:
-                asset_to_move = self.scan_and_remove_from_inventory(name)
-                if asset_to_move:
-                    self.rig.store_to_rig(asset_to_move)
-                    print(f"Moved '{name}' to {self.rig.name}.")
+                self.rig.repair()
+        else:
+            print(f"{self.name} has no CryptoToken to perform repairs.")
 
-        def retrieve_from_rig(self, name):  # DONE
-            if self.rig:
-                asset_to_retrieve = self.rig.release_from_rig(name)
-                if asset_to_retrieve:
-                    self.inventory.append(asset_to_retrieve)
-                    print(f"Retrieved '{name}' from {self.rig.name}.")
+    def move_to_rig(self, name):  # DONE
+        if self.rig:
+            asset_to_move = self.scan_and_remove_from_inventory(name)
+            if asset_to_move:
+                self.rig.store_to_rig(asset_to_move)
+                print(f"Moved '{name}' to {self.rig.name}.")
+
+    def retrieve_from_rig(self, name):  # DONE
+        if self.rig:
+            asset_to_retrieve = self.rig.release_from_rig(name)
+            if asset_to_retrieve:
+                self.inventory.append(asset_to_retrieve)
+                print(f"Retrieved '{name}' from {self.rig.name}.")
--- Rig.py
+++ Rig.py
@@ -22,88 +22,88 @@ class Rig:
 
         self.__level = 0
 
-        def get_name(self):
-            return self.__name
+    def get_name(self):
+        return self.__name
 
-        def get_damage_counter(self):
-            return self.__damage_couter
+    def get_damage_counter(self):
+        return self.__damage_couter
 
-        def get_broken_state(self):
-            return self.__broken_state
+    def get_broken_state(self):
+        return self.__broken_state
 
-        def set_damage_counter(self, counter):
-            self.__damage_couter = counter
+    def set_damage_counter(self, counter):
+        self.__damage_couter = counter
 
-        def set_broken_state(self, state):
-            self.__broken_state = state
+    def set_broken_state(self, state):
+        self.__broken_state = state
 
-        def get_storage(self):
-            return self.__storage
+    def get_storage(self):
+        return self.__storage
 
-        def get_level(self):
-            return self.__level
+    def get_level(self):
+        return self.__level
 
-        def set_level(self, level):
-            self.__level = level
+    def set_level(self, level):
+        self.__level = level
 
-        # properties for instance attributed
-        name = property(get_name)
-        damage_counter = property(get_damage_counter, set_damage_counter)
-        broken_state = property(get_broken_state, set_broken_state)
-        storage = property(get_storage)
-        level = property(get_level, set_level)
+    # properties for instance attributed
+    name = property(get_name)
+    damage_counter = property(get_damage_counter, set_damage_counter)
+    broken_state = property(get_broken_state, set_broken_state)
+    storage = property(get_storage)
+    level = property(get_level, set_level)
 
-        def repair(self):
-            if self.damage_counter > 0:
-                self.damage_counter = 0
-                self.broken_state = False
-                print(f"{self.name} has been repaired.")
-            else:
-                print(f"{self.name} does not require any repairs.")
+    def repair(self):
+        if self.damage_counter > 0:
+            self.damage_counter = 0
+            self.broken_state = False
+            print(f"{self.name} has been repaired.")
+        else:
+            print(f"{self.name} does not require any repairs.")
 
-        def upgrade(self):
-            self.level += 1
+    def upgrade(self):
+        self.level += 1
 
-        def take_hit(self):
-            damage_threshold = 2 + self.level
-            self.damage_counter += 1
-            if self.damage_counter >= damage_threshold:
-                self.broken_state = True
+    def take_hit(self):
+        damage_threshold = 2 + self.level
+        self.damage_counter += 1
+        if self.damage_counter >= damage_threshold:
+            self.broken_state = True
 
-        def generate_asset(self):
-            possible_assets = [
-                Asset("CryptoToken", "Used to acquire or repair rigs"),
-                Asset("Data Spike", "Used in battles"),
-                Asset("Removable Drive", "Found in rigs and used for "
-                                         "extraction"),
-                Asset("Security Chip", "Used to encrypt or decrypt assets"),
-                Asset("Hardware Patch", "Used to upgrade rigs")
-            ]
-            new_asset = random.choice(possible_assets)
-            self.__storage.append(new_asset)
+    def generate_asset(self):
+        possible_assets = [
+            Asset("CryptoToken", "Used to acquire or repair rigs"),
+            Asset("Data Spike", "Used in battles"),
+            Asset("Removable Drive", "Found in rigs and used for "
+                                     "extraction"),
+            Asset("Security Chip", "Used to encrypt or decrypt assets"),
+            Asset("Hardware Patch", "Used to upgrade rigs")
+        ]
+        new_asset = random.choice(possible_assets)
+        self.__storage.append(new_asset)
 
-        def store_to_rig(self, asset):  # DONE
-            self.storage.append(asset)
+    def store_to_rig(self, asset):  # DONE
+        self.storage.append(asset)
 
-        def release_from_rig(self, name):  # DONE
-            for asset in self.storage:
-                if asset.asset_name == name and not asset.encrypted:
-                    self.storage.remove(asset)
-                    return asset
-            return None
+    def release_from_rig(self, name):  # DONE
+        for asset in self.storage:
+            if asset.asset_name == name and not asset.encrypted:
+                self.storage.remove(asset)
+                return asset
+        return None
 
-        def rig_condition(self):
-            if not self.broken_state:
-                return f"Broken (Level {self.level})"
+    def rig_condition(self):
+        if not self.broken_state:
+            return f"Broken (Level {self.level})"
 
-            condition_names = ["Novice", "Silver", "Pristine", "Gold",
-                               "Platinum", "Diamond"]  # Level 0 to 5
+        condition_names = ["Novice", "Silver", "Pristine", "Gold",
+                           "Platinum", "Diamond"]  # Level 0 to 5
 
-            if 0 <= self.level < len(condition_names):
-                status = condition_names[self.level]
-            else:
-                status = "Advanced"
-            return f"{status} (Level {self.level})"
+        if 0 <= self.level < len(condition_names):
+            status = condition_names[self.level]
+        else:
+            status = "Advanced"
+        return f"{status} (Level {self.level})"
 
-        def __str__(self):
-            pass
+    def __str__(self):
+        pass
